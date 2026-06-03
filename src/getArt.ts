c481516e4e8fbe2aeb9a057f1eaebba8ff723ca7
Sirisha Meka <sirishameka@Mac.lan>
2026-06-02 21:41:30 -0400

    code review

--- src/getArt.ts
+++ src/getArt.ts
@@ -1,5 +1,6 @@
 import { ImageType, Response } from "./types"
 
+// added join at the declaration level to avoid invocation at every artFetcher call 
 const fields = [
     'id',
     '_score',
@@ -9,9 +10,12 @@ const fields = [
 ].join(',')
 
 export const artFetcher = async (search: string = '') => {
+
+    // URL sanitization using native method instaed of regex
     const params = new URLSearchParams({ q: search, fields })
     const url = `https://api.artic.edu/api/v1/artworks/search?${params}`
 
+    // Http error handling to avoid downstream crash errors handling undefined data in case of errors
     const r = await fetch(url)
     if (!r.ok) throw new Error(`API error ${r.status}`)
     const json = await r.json()
